f20716ccda3962541c941982575100deda74999a
Philip I. Thomas <pthomas@opendns.com>
2013-08-06 16:36:43 -0700

    tweaks

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-    $('.home .logo').hide().delay(500).fadeIn(2000);
-    $('.home-nav').hide().delay(3000).fadeIn(1500);
-    $('.content').hide().fadeIn(500);
+    $('.home-nav').hide().delay(1000).fadeIn(1500);
     console.log('Fork on Github: http://github.com/philipithomas/www.philipithomas.com')
 });
