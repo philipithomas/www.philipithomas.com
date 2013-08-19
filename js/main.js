372057ba8d011bfcf589e4f6899f189f0b214c8e
Philip I. Thomas <pthomas@opendns.com>
2013-08-19 00:01:49 -0700

    progress

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
-    $('.home-nav').hide().delay(1000).fadeIn(1500);
+    
+    $('.home-nav').hide().fadeIn(1500);
     console.log('Fork on Github: http://github.com/philipithomas/www.philipithomas.com')
+    
+    var $sideBar = $('.sidebar')
+
+    $('.sidebar').affix({
+        offset: 100
+    });
 });
+
