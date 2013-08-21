2989553b5ca8d2291780c8445e5d7eee13cc7cbe
Philip I. Thomas <pthomas@opendns.com>
2013-08-20 18:52:57 -0700

    Layout tweaks

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-    
-    $('.home-nav').hide().fadeIn(1500);
     console.log('Fork on Github: http://github.com/philipithomas/www.philipithomas.com')
     
     var $sideBar = $('.sidebar')
