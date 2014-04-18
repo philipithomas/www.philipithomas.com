50861f6b4b846939f700cab1fc93504f252c733c
Philip I. Thomas <mail@philipithomas.com>
2014-04-17 21:03:48 -0700

    Pretty sidebar achieved. Pushing before adding the menu itself for testing.

---
+++ _assets/javascripts/custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){    
+  var snapper = new Snap({
+      element: document.getElementById('main-wrapper'),
+      hyperextensible: false,
+      disable: 'left',
+  });
+  $( window ).resize(function() {
+    if ($( window ).width() >= 768) {
+      snapper.close();
+      snapper.disable();
+    } else {
+      snapper.enable();
+    }
+  });
+
+  $("#sidebar-toggle").click( function() {
+    if( snapper.state().state=="right" ){
+      snapper.close();
+    } else {
+      snapper.open('right');
+    }
+  });
+});
