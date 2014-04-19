c23900c4430dd8f06c523ea37bfb8551c1b38dfc
Philip I. Thomas <mail@philipithomas.com>
2014-04-18 19:27:57 -0700

    Fix the fixed position (lol) of the sidebar toggle

--- _assets/javascripts/custom.js
+++ _assets/javascripts/custom.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){    
-  
   // 300ms delays fucking suck
   $(function() {
       FastClick.attach(document.body);
@@ -19,11 +18,21 @@ $(document).ready(function(){
     }
   });
 
+  // handle toggle button
   $("#sidebar-toggle").click( function() {
-    if( snapper.state().state=="right" ){
+    if(snapper.state().state=="right" ){
       snapper.close();
     } else {
       snapper.open('right');
     }
+  });
+
+  // Due to fixed position, make toggle disappear  
+  snapper.on('open', function() {
+      $('#sidebar-toggle').hide();
+  });
+  snapper.on('close', function() {
+    console.log('close');
+      $('#sidebar-toggle').delay(200).show(100);
   });
 });
