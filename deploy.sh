393d671b79831adab5c02c35c28834ad66d95455
Philip I. Thomas <mail@philipithomas.com>
2014-05-20 22:20:55 -0700

    Clean up deployment

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+# Script for deploying a jekyll build to AWS
 
+# override cache
 if [ "$environment" != "production" ]; then
   echo "Overriding robots.txt for a $environment environment"
   rm _site/robots.txt
   echo "User-agent: * \nDisallow: /" > _site/robots.txt
 fi
+
+# Push the build using the s3_website.yml settings
 bundle exec s3_website push --headless
 
+# Clear the Cloudflare cache
 curl https://www.cloudflare.com/api_json.html \
     -d 'a=fpurge_ts' \
     -d "tkn=$cloudflare_token" \
     -d "email=$cloudflare_email" \
-    -d 'z=philipithomas.com' \
+    -d "z=$cloudflare_zone" \
     -d 'v=1'
