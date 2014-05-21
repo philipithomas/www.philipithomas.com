8396a0adcbad4707bedf48acf406917ba48bbc23
Philip I. Thomas <mail@philipithomas.com>
2014-05-20 21:36:09 -0700

    redo deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
+
+if ["$environment" !== "production"]; then
+  rm _site/robots.txt
+  echo "User-agent: * \nDisallow: /" > _site/robots.txt
+fi
+bundle exec s3_website push --headless
+
 curl https://www.cloudflare.com/api_json.html \
     -d 'a=fpurge_ts' \
     -d "tkn=$cloudflare_token" \
     -d "email=$cloudflare_email" \
     -d 'z=philipithomas.com' \
     -d 'v=1'
-
