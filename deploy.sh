648339b8e7ffb5acebc2a42766a8368383031a67
Philip I. Thomas <mail@philipithomas.com>
2015-11-22 20:28:59 -0800

    config needs updating

--- deploy.sh
+++ deploy.sh
@@ -30,6 +30,8 @@ then
     else
         # Push to staging domain. 
         bucket="stage.philipithomas.com"
+        sed -i 's/https:\/\/www.philipithomas.com/https:\/\/stage.philipithomas.com/g' config.yaml
+
 
         # Don't let search engines see the stage
         rm public/robots.txt
